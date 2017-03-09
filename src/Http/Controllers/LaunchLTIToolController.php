5ec556369cff089ba1b822d8d707d464cd1fc171
Josh <jharing10@Joshs-MacBook-Pro.local>
2017-03-09 10:11:27 +0200

    link for runtime

---
+++ src/Http/Controllers/LaunchLTIToolController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EONConsulting\LaravelLTI\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+
+class LaunchLTIToolController extends LTIBaseController {
+
+    public function launch(Request $request) {
+
+        $launch_url = $request->get('launch_url', '');
+        $key = $request->get('key', '');
+        $secret = $request->get('secret', '');
+
+        return laravel_lti()->launch($launch_url, $key, $secret);
+    }
+
+}
--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,6 +3,8 @@
 Route::group(['middleware' => ['web'], 'prefix' => '/eon/lti', 'namespace' => 'EONConsulting\LaravelLTI\Http\Controllers'], function() {
     Route::group(['middleware' => ['auth']], function() {
 
+        Route::get('/launch', ['as' => 'eon.laravellti.launch', 'uses' => 'LaunchLTIToolController@launch']);
+
         // list all of the api's
         Route::get('/install', ['as' => 'eon.laravellti.install', 'uses' => 'InstallLTIToolController@index']);
         Route::post('/install', ['as' => 'eon.laravellti.install', 'uses' => 'InstallLTIToolController@store']);
